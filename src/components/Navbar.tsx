f4b7c224fc90df8f48276e87dea15ec0520baea2
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-21 00:24:44 +0530

    fix: Scroll issue fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Heading, Button } from "@chakra-ui/react";
 
 function Navbar() {
   return (
-    <nav className="flex flex-1 justify-between items-center drop-shadow-md border-b px-5 py-2.5  fixed top-0 w-full h-24 box-border bg-lightBlue">
+    <nav className="flex flex-1 justify-between items-center drop-shadow-md border-b px-5 py-2.5  fixed top-0 w-full h-24 box-border bg-lightBlue z-10">
       <Link to="/">
         <Heading fontSize={"large"} fontWeight={"600"}>
           TimelyAI
