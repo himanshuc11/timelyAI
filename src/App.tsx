b7cbec85ec0938016bce3dce6487c963c88ec9c7
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-21 12:57:44 +0530

    fix: Fixed background-color issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="h-full min-w-screen flex flex-col">
+    <div className="h-full min-w-screen flex flex-col bg-solitude">
       <Navbar />
-      <div className="h-24 w-full pt-24" />
+      <div className="h-24 w-full pt-24 bg-solitude" />
       <main className="h-full w-full flex flex-col bg-solitude">
         <Outlet />
       </main>
